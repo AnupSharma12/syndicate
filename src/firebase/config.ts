cfea5edd86ead91b8220ce7ea692b0d5a0bb2064
AnupSharma12 <anupsharma2065@gmail.com>
2025-12-11 21:11:01 +0545

    chore: Update Firebase configuration to syndicate-esp project

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-5498473461-21802",
-  "appId": "1:533928397862:web:4d59d3bd12ebb7bd81aae7",
-  "apiKey": "AIzaSyDummN2FZKenfrrxIOJg4IP56tKodfGWY8",
-  "authDomain": "studio-5498473461-21802.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "533928397862"
+  apiKey: "AIzaSyBZgNCmeBvLplVk_YTcIR7hYgh1nu8pwgo",
+  authDomain: "syndicate-esp.firebaseapp.com",
+  projectId: "syndicate-esp",
+  storageBucket: "syndicate-esp.firebasestorage.app",
+  messagingSenderId: "935445395288",
+  appId: "1:935445395288:web:99c47c66c6f9f4b228d72d",
+  measurementId: "G-F3FS7VMGD7"
 };
